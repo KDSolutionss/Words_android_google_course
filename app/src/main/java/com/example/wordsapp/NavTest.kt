5cfad9a1741793107378c6f670263cdb4eb23a55
kondr <kondratovdany@gmail.com>
2023-05-18 21:04:52 +0500

    Initial commit

---
+++ app/src/main/java/com/example/wordsapp/NavTest.kt
@@ -0,0 +1,18 @@
+package com.example.wordsapp
+
+
+
+
+
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class NavTest {
+
+    fun navigate_to_words_nav_component()
+    {
+
+    }
+}
